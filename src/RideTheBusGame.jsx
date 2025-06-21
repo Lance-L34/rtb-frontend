7a29a338f5f903ab16ee9bd285608fa5a84c6928
Lance <lclam230@gmail.com>
2025-06-21 18:03:16 -0500

    Fix syntax: socket URL string

--- src/RideTheBusGame.jsx
+++ src/RideTheBusGame.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
-
-const socket = io(https://rtb-backend1.onrender.com);
+const socket = io("https://rtb-backend1.onrender.com");
 
 export default function RideTheBusGame() {
   const [username, setUsername] = useState('');
