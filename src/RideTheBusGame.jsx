1ca20161dea1f9c15a946175efc93512153070b9
Lance <lclam230@gmail.com>
2025-06-21 17:54:33 -0500

    Connect frontend to deployed backend

--- src/RideTheBusGame.jsx
+++ src/RideTheBusGame.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // Change to your backend URL
+const socket = io(https://rtb-backend1.onrender.com);
 
 export default function RideTheBusGame() {
   const [username, setUsername] = useState('');
